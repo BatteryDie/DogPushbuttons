6b03d2b17160683b28f0122aa91bd12c39bec477
BatteryDie <BatteryDie@users.noreply.github.com>
2021-11-16 09:26:01 -0500

    add sound

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ while True:
     text = input("Debug Command:")
     if text == "bathroom":
         print("Bathroom!")
-        my_sound = pygame.mixer.Sound('sound/bathroom.mp3')
+        my_sound = pygame.mixer.Sound('sounds/bathroom.mp3')
         my_sound.play()
         client.send_message("Bathroom!", title="Dog Pushbuttons")
     if text == "outside":
         print("Outside!")
-        my_sound = pygame.mixer.Sound('sound/outside.mp3')
+        my_sound = pygame.mixer.Sound('sounds/outside.mp3')
         my_sound.play()
         client.send_message("Outside!", title="Dog Pushbuttons")
     if text == "water":
         print("Water!")
-        my_sound = pygame.mixer.Sound('sound/water.mp3')
+        my_sound = pygame.mixer.Sound('sounds/water.mp3')
         my_sound.play()
         client.send_message("Water!", title="Dog Pushbuttons")
     if text == "food":
         print("Food!")
-        my_sound = pygame.mixer.Sound('sound/food.mp3')
+        my_sound = pygame.mixer.Sound('sounds/food.mp3')
         my_sound.play()
         client.send_message("Food!", title="Dog Pushbuttons")
     if text == "testsound":
         print("Testing sound...")
-        my_sound = pygame.mixer.Sound('sound/test.mp3')
+        my_sound = pygame.mixer.Sound('sounds/test.mp3')
         my_sound.play()
     if text == "testalert":
         print("Testing alert...")
