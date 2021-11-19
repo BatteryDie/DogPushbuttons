f67a8268320b3f359ffc07de826d2786f5cb128a
Luca Jones <BatteryDie@users.noreply.github.com>
2021-11-19 11:54:59 -0500

    Update main_pi.py

--- main_pi.py
+++ main_pi.py
@@ -2,34 +2,40 @@ from gpiozero import Button
 from pushover import Client  # For sending alert
 import time
 import pygame  # For playing sound
-pygame.init()
+pygame.init() # Load pygame - no gui
 
 client = Client("ur7vskev12zazdf6twu9dt5a4wy7pf", api_token="aqdqexpqcpagiazt1a386xjnp4mgw4")
 
-bath_button = Button(14,None,True)
-out_button = Button(15,None,True)
-water_button = Button(16,None,True)
-food_button = Button(17,None,True)
+bath_button = Button(19,None,True)
+out_button = Button(26,None,True)
+water_button = Button(20,None,True)
+food_button = Button(21,None,True)
 
 while True:
+    localtime = time.asctime( time.localtime(time.time()) )
     if bath_button.is_pressed:
-        print("Bathroom!")
+        print(localtime+" Bathroom!")
         my_sound = pygame.mixer.Sound('sounds/bathroom.mp3')
         my_sound.play()
         client.send_message("Bathroom!", title="Dog Pushbuttons")
+        time.sleep(1.0)
     if out_button.is_pressed:
-        print("Outside!")
+        print(localtime+" Outside!")
         my_sound = pygame.mixer.Sound('sounds/outside.mp3')
         my_sound.play()
         client.send_message("Outside!", title="Dog Pushbuttons")
+        time.sleep(1.0)
     if water_button.is_pressed:
-        print("Water!")
+        print(localtime+" Water!")
         my_sound = pygame.mixer.Sound('sounds/water.mp3')
         my_sound.play()
         client.send_message("Water!", title="Dog Pushbuttons")
+        time.sleep(1.0)
     if food_button.is_pressed:
-        print("Food!")
+        print(localtime+" Food!")
         my_sound = pygame.mixer.Sound('sounds/food.mp3')
         my_sound.play()
         client.send_message("Food!", title="Dog Pushbuttons")
-    time.sleep(0.1)
+        time.sleep(1.0)
+
+
